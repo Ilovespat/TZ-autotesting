bc793e98aa485f0d0dd504254a913b8686116516
Sonya <144343042+Ilovespat@users.noreply.github.com>
2023-11-15 16:28:59 +0200

    Фикстура, инициализация webdriver

--- conftest.py
+++ conftest.py
@@ -1,11 +1,11 @@
 import pytest
 from selenium import webdriver
 
-# Фикстура, инициализация webdriver и закрытие браузера после теста 
+# Фикстура, инициализация webdriver и закрытие браузера после теста
 @pytest.fixture
-def browser():
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(10)
-    yield browser
-    browser.close()
+def browser(): #  фикстурная функция pytest
+    browser = webdriver.Chrome() # Все вызовы WebDriver будут выполняться через объект драйвера
+    browser.implicitly_wait(10) # неявное ожидание до появления элемента
+    yield browser # ф-я генератор, конструкция разделяет ф-ю на до и поле тестов
+    browser.close() # завершение сессии и закрытие браузера
 
