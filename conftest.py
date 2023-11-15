a173ac5f7262be99c2d932cd44f00af0ffa2943d
Sonya <144343042+Ilovespat@users.noreply.github.com>
2023-11-15 16:25:51 +0200

    Фикстура для инициализации webdriver.

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
 
-# Фикстура, инициализация webdriver и закрытие браузера после теста
+# Фикстура, инициализация webdriver и закрытие браузера после теста 
 @pytest.fixture
 def browser():
     browser = webdriver.Chrome()
